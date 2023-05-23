e1861b5159a15ed77015bc575165593991e43537
Albert Urbina <johan50693@gmail.com>
2023-05-23 18:46:09 -0400

    Configuracion de opciones en el drawer navigator

--- src/screens/Pagina1Screen.tsx
+++ src/screens/Pagina1Screen.tsx
@@ -1,14 +1,28 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, Button} from 'react-native';
-import {StackScreenProps} from '@react-navigation/stack';
+// import {StackScreenProps} from '@react-navigation/stack';
 import { styles } from '../theme/appTheme';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { DrawerScreenProps } from '@react-navigation/drawer';
 
-interface Props extends StackScreenProps<any,any>{}
+// interface Props extends StackScreenProps<any,any>{}
+interface Props extends DrawerScreenProps<any,any>{}
 
 
 export const Pagina1Screen = ( {navigation}: Props) => {
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Button
+          title='Menu'
+          onPress={ () => navigation.toggleDrawer()}
+        />
+      )
+    });    
+  }, [])
+  
 
   return (
       <View style={styles.globalMargin}>
