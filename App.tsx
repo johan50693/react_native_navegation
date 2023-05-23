fdc8ddea59d63a7156587e63fba81cca6ab7f0ce
Albert Urbina <johan50693@gmail.com>
2023-05-23 18:15:47 -0400

    Ajuste para el manejo del drawer con react-native-reanimated@2.14.4 y babel.config.js

--- App.tsx
+++ App.tsx
@@ -3,11 +3,13 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './src/navigator/StackNavigator';
+import { MenuLateralBasico } from './src/navigator/MenuLateralBasico';
 
 export const App = () => {
   return (
     <NavigationContainer>
-        <StackNavigator/>
+        {/* <StackNavigator/> */}
+        <MenuLateralBasico />
     </NavigationContainer>
   );
 };
---
+++ src/navigator/MenuLateralBasico.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { StackNavigator } from './StackNavigator';
+import { SettingsScreen } from '../screens/SettingsScreeen';
+
+const Drawer = createDrawerNavigator();
+
+export const MenuLateralBasico = () =>  {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="StackNavigator" component={StackNavigator} />
+      <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+    </Drawer.Navigator>
+  );
+}
