88e1ef4e379c4bfac619d23e19abd304edf16f70
Albert Urbina <johan50693@gmail.com>
2023-06-10 18:48:58 -0400

    Definir espacio para el icono en el tab

--- src/navigator/Tabs.tsx
+++ src/navigator/Tabs.tsx
@@ -5,6 +5,7 @@ import { Tab2Screen } from '../screens/Tab2Screen';
 import { Tab3Screen } from '../screens/Tab3Screen';
 import { StackNavigator } from './StackNavigator';
 import { colors } from '../theme/appTheme';
+import { Text } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,15 +15,41 @@ export const Tabs = () => {
     sceneContainerStyle = {{
       backgroundColor: 'white',
     }} 
-    screenOptions= {{
+    screenOptions= {({route}) => ({
         tabBarActiveTintColor: colors.primary,
         tabBarStyle: {
           borderTopColor: colors.primary,
           borderTopWidth: 0,
           elevation:0
+        },
+        tabBarLabelStyle: {
+          fontSize: 15
+        },
+        tabBarIcon: ({color,focused,size}) => {
+          // console.log(route);
+
+          let iconName: String = '';
+
+          switch (route.name) {
+            case 'Tab1':
+              iconName = 'T1';
+              break;
+            
+            case 'Tab2':
+              iconName = 'T2';
+              break;
+
+            case 'StackNavigator':
+              iconName = 'St';
+              break;
+          
+          }
+          return <Text style= {{color}} >{iconName}</Text>
         }
-      }}
+      })}
+      
     >
+      {/* <Tab.Screen name="Tab1" options={{title: 'Tab1', tabBarIcon: (props) => <Text style={{color: props.color}} >Text1</Text>}} component={Tab1Screen} /> */}
       <Tab.Screen name="Tab1" options={{title: 'Tab1'}} component={Tab1Screen} />
       <Tab.Screen name="Tab2" options={{title: 'Tab2'}} component={Tab2Screen} />
       <Tab.Screen name="StackNavigator" options={{title: 'Stack'}} component={StackNavigator} />
