a83fd61a32108c5de34c138e659d9afa74e24af7
Albert Urbina <johan50693@gmail.com>
2023-06-11 22:24:43 -0400

    Personalizar material top tab navigator

--- src/navigator/TopTapNavigator.tsx
+++ src/navigator/TopTapNavigator.tsx
@@ -3,12 +3,61 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import { ChatScreen } from '../screens/ChatScreen';
 import { ContactsScreen } from '../screens/ContactsScreen';
 import { AlbumsScreen } from '../screens/AlbumsScreen';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { colors } from '../theme/appTheme';
+import { Text } from 'react-native';
 
 const Tab = createMaterialTopTabNavigator();
 
+
 export const TopTabNavigator = () => {
+  
+  const {top} =useSafeAreaInsets();
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      style={{
+        paddingTop: top
+      }}
+
+      sceneContainerStyle = {{
+        backgroundColor: 'white'
+      }}
+
+      screenOptions={({route}) => ({
+        tabBarPressColor: colors.primary,
+        tabBarShowIcon: true,
+        tabBarIndicatorStyle: {
+          backgroundColor: colors.primary,
+        },
+        tabBarStyle: {
+          borderTopColor: colors.primary,
+          borderTopWidth: 0,
+          elevation:0
+        },
+        tabBarIcon: ({color,focused}) => {
+          // console.log(route);
+
+          let iconName: String = '';
+
+          switch (route.name) {
+            case 'Chat':
+              iconName = 'Ch';
+              break;
+            
+            case 'Contacts':
+              iconName = 'Co';
+              break;
+
+            case 'Albums':
+              iconName = 'Al';
+              break;
+          
+          }
+          return <Text style= {{color}} >{iconName}</Text>
+        }
+      })}
+    >
       <Tab.Screen name="Chat" component={ChatScreen} />
       <Tab.Screen name="Contacts" component={ContactsScreen} />
       <Tab.Screen name="Albums" component={AlbumsScreen} />
