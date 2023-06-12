4ca002632cc4fb38c7b2d5ab5b783116fbfbed08
Albert Urbina <johan50693@gmail.com>
2023-06-12 16:29:43 -0400

    Se disspara accion de signIn y se hace validacion para ocultar el boton

--- src/screens/ContactsScreen.tsx
+++ src/screens/ContactsScreen.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useContext} from 'react';
+import { View, Text, Button } from 'react-native';
+import { styles } from '../theme/appTheme';
+import { AuthContext } from '../context/AuthContext';
 export const ContactsScreen = () => {
+
+  const {signIn,authState} = useContext(AuthContext);
+
   return (
-      <View>
-        <Text> ContactsScreen </Text>
+      <View style={ styles.globalMargin} >
+        <Text style={styles.title} > ContactsScreen </Text>
+
+        {
+          (authState.isLoggedin === false) 
+            ? <Button
+                title='signIn'
+                onPress={ signIn}
+              /> 
+          : null
+        }
       </View>
   );
 };
