663bda63ee117cca4fa62dd114617c3456654f22
Albert Urbina <johan50693@gmail.com>
2023-06-12 16:49:31 -0400

    touchableicon componente reutilizable

---
+++ src/components/TouchableIcon.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+import { colors } from '../theme/appTheme';
+
+interface Props {
+  iconName: string;
+}
+export const TouchableIcon = ({iconName}:Props) => {
+  return (
+    <TouchableOpacity
+      onPress={ () => console.log(iconName)}
+    >
+      <Icon 
+        name={iconName} 
+        size={30} 
+        color={colors.primary}
+      />
+    </TouchableOpacity>
+  )
+}
--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 import React, {useEffect} from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { styles } from '../theme/appTheme';
+import { TouchableIcon } from '../components/TouchableIcon';
 
 export const Tab1Screen = () => {
 
@@ -13,8 +14,8 @@ export const Tab1Screen = () => {
   return (
       <View style={styles.globalMargin} >
         <Text style={styles.title} > Icons </Text>
-        <Icon name="rocket" size={30} color="#900" />
-        <Icon name="airplane-outline" size={30} color="#900" />
+        <TouchableIcon iconName="rocket" />
+        <TouchableIcon iconName="airplane-outline" />
       </View>
   );
 };
